cf075da92f9ed9c21ce65f2968f8309ad3db7699
frickp <peter.l.frick@gmail.com>
2015-08-07 09:47:29 -0700

    bam and bed files now locally available

--- learningArgParse.py
+++ learningArgParse.py
@@ -15,6 +15,9 @@ parser.add_argument('strings',type=str,nargs='+',help="This is a variable that I
 parser.add_argument('--capitalize','-c',help='Capitalize the string',action='store_true')
 parser.add_argument('--nospace','-ns',help='Prints the string with no spaces',action='store_true')
 parser.add_argument('--rickroll','-r',help='I think you know what will happen...',action='store_true')
+parser.add_argument('--signature','-s',help='string appended to return',default='Sincerely, argbot')
+parser.add_argument('--greeting','-g',help='string prefix to return',required=True)
+
 
 args=parser.parse_args()
 
@@ -29,7 +32,15 @@ if args.nospace:
 else:
         a=' '.join(a)
 
+if args.signature:
+	a=a + '\n' + args.signature
+
+if args.greeting:
+	a=args.greeting.upper() + ', \n' + a
+
 if args.capitalize:
 	a=a.upper()
+
+
 
 print a
--- makeSampleSheet/MakeMiseqSampleSheet.py
+++ makeSampleSheet/MakeMiseqSampleSheet.py
@@ -1,166 +1,90 @@
-
-# coding: utf-8
-
-# In[76]:
-
+#!/usr/bin/env python
 #This script is designed to automatically generate the primer barcode combinations for scATAC experiments
+#Usage: MakeMiseqSampleSheetINFLUX.py -sc -i miseq -r 5 -c 7 -b 'PC9test'
 
 import pandas as pd
 import numpy as np
 import datetime
+import argparse
+import sys
+
 table=pd.read_csv('MiSeqSampleTemplate.txt',delimiter='\t',skiprows=19)
 Barcodes=pd.read_csv('NexteraBarcodeInfo2.txt',delimiter='\t',index_col=0)
 
 
-# In[77]:
+parser = argparse.ArgumentParser(description="Description: This script automatically generates Nextera sequencing primer combinations. **Note, Ad1MX is indexed as '0'")
 
-#Barcodes.head()
+if len(sys.argv)<2:
+	print "need to add arguments!"
+	print "type [scriptName.py] -h for documentation"
+	quit()
 
+parser.add_argument('--singleCell','-sc',help='True/False flag for if data is single-cell or not. Still need to add bulk ATAC functionality',action='store_true',required=True)
+parser.add_argument('--instrument','-i',help='Enter *exact* name of Illumina instrument: "miseq" or "nextseq" ',required=True)
+parser.add_argument('--rowNum','-r',help='Row number for ad1 (i5)',required=True)
+parser.add_argument('--colNum','-c',help='Col number for ad2 (i7)',required=True)
+parser.add_argument('--baseName','-b',help='Basename for cell names',required=True)
+parser.add_argument('--saveDir','-s',help='directory to save the primer sheet to',default='./')
+parser.add_argument('--fileName','-f',help='Name of primer sheet',default='PrimerSheet.csv')
 
-# In[78]:
+args=parser.parse_args()
 
-#Barcodes[(Barcodes.Name=='N502') & (Barcodes.Instrument=='MiSeq')]
+args.instrument=args.instrument.lower()
+if args.instrument not in ['miseq', 'nextseq']:
+	print "Make sure the instrument name exactly reads: 'miseq' or 'nextseq'"
+	quit()
 
-
-# In[79]:
-
-# I need to pull in variables from the user using argparse. What I need are the following
-# version (v1 or v2)
-# instrument (Miseq or NextSeq)
-# Ad1Number (Row#)
-# Ad2Number (Col#)
 # I should make a note that Ad1MX is indexed as '0'
 
-
-# In[80]:
-
-#If single-cell (generate dictionaries of primers):
-#ad2_dict=dict(Barcodes[(Barcodes.Orientation=='i7_ad2') & (Barcodes.Version=='v2') & (Barcodes['Instrument'].str.contains('NextSeq'))]['Sequence'])
-#ad1_dict=dict(Barcodes[(Barcodes.Orientation=='i5_ad1') & (Barcodes.Version=='v2') & (Barcodes['Instrument'].str.contains('NextSeq'))]['Sequence'])
-#The top two work...
-
 #ad2_dict=dict(Barcodes[(Barcodes.Orientation=='i7_ad2') & (Barcodes.Version=='v2') & (Barcodes['Instrument'].str.contains('NextSeq'))]['Name','Sequence'])
-ad1_dict=dict(Barcodes[(Barcodes.Orientation=='i5_ad1') & (Barcodes.Version=='v2') & (Barcodes['Instrument'].str.contains('NextSeq'))])
-ad2_dict=dict(Barcodes[(Barcodes.Orientation=='i7_ad2') & (Barcodes.Version=='v2') & (Barcodes['Instrument'].str.contains('NextSeq'))])
+if args.instrument=='nextseq':
+	ad1_dict=dict(Barcodes[(Barcodes.Orientation=='i5_ad1') & (Barcodes.Version=='v2') & (Barcodes['Instrument'].str.contains('NextSeq'))])
+	ad2_dict=dict(Barcodes[(Barcodes.Orientation=='i7_ad2') & (Barcodes.Version=='v2') & (Barcodes['Instrument'].str.contains('NextSeq'))])
+else:
+	ad1_dict=dict(Barcodes[(Barcodes.Orientation=='i5_ad1') & (Barcodes.Version=='v2') & (Barcodes['Instrument'].str.contains('MiSeq'))])
+	ad2_dict=dict(Barcodes[(Barcodes.Orientation=='i7_ad2') & (Barcodes.Version=='v2') & (Barcodes['Instrument'].str.contains('MiSeq'))])
 
-
-# In[81]:
-
-#ad1_dict['Sequence'].loc[5]
-
-
-# In[82]:
-
-#Index by string finding
-#Barcodes[Barcodes['Instrument'].str.contains('NextSeq')]
-
-
-# In[83]:
-
-#Preliminary code combined into working code segment below
-#b=[i*12+1 for i in range(8)]
-#x=[]
-#for i in b:
-#    x.append(a[i])
-#print x
-
-
-# In[84]:
 
 # This is correctly formatted for ad2_i7
-colNum = 5 #Make this a variable to be read in by argparse
+#colNum = 5 #Make this a variable to be read in by argparse
 x  = []    #change x to ad1
 x2 = []
 x3 = []
-for i in [i*12+colNum for i in range(8)]:
+for i in [i*12+int(args.colNum) for i in range(8)]:
     x.append(i)
     x2.append(ad2_dict['Sequence'].loc[i])
     x3.append(ad2_dict['Name'].loc[i])
 x=x*12
 x2=x2*12
 x3=x3*12
-#print x
-#print type(x)
-#print len (x)
-#print x2
-#print x3
-
-#ad1_dict['Sequence'].loc[5]
-
-
-# In[85]:
 
 # This is correctly formatted for ad1_i5
-RowNum = 5 #Make this a variable to be read in by argparse
+#RowNum = 5 #Make this a variable to be read in by argparse
 y  = []
 y2 = []
 y3 = []
 for i in [x+1 for x in range(8)]:
     for j in [x+1 for x in range(12)]:
-        y.append(j+12*(RowNum-1)) 
-        y2.append(ad1_dict['Sequence'].loc[j+12*(RowNum-1)])
-        y3.append(ad1_dict['Name'].loc[j+12*(RowNum-1)])
-#print y
-#print type(y)
-#print len(y)
-#print y2
-#print y3
+        y.append(j+12*(int(args.rowNum)-1)) 
+        y2.append(ad1_dict['Sequence'].loc[j+12*(int(args.rowNum)-1)])
+        y3.append(ad1_dict['Name'].loc[j+12*(int(args.rowNum)-1)])
 
-#ad1_dict['Sequence'].loc[5]
-
-
-# In[86]:
-
-# Now I need to figure out how to take x2 and y2 and put them in a new dataframe
-# Make the indexing column the same as the one from 
-#d = pd.DataFrame(data='index')
-
-
-# In[87]:
-
-#Barcodes.index.values
-
-
-# In[88]:
-
-CommonName = 'CellID-'
 cols = ['Sample_ID','Sample_Name','Sample_Well','I7_Index_ID','index','I5_Index_ID','index2','Sample_Project','Description']
 idx = [i +1 for i in range(96)]
-sampleNames = [CommonName+str(i + 1) for i in range(96)]
-
-
-# In[89]:
+sampleNames = [args.baseName + str(i + 1) for i in range(96)]
 
 PrimerSheet=pd.DataFrame(columns=cols,index=idx)
-
-#PrimerSheet.head()
-
-
-# In[90]:
 
 PrimerSheet['Sample_ID']=sampleNames
 PrimerSheet['I5_Index_ID']=y2
 PrimerSheet['index']=y3
 PrimerSheet['I7_Index_ID']=x2
 PrimerSheet['index2']=x3
-#PrimerSheet.head()
-
-
-# In[91]:
-
-#I think that all that's left is to interface the script with argparse!!
-
-
-# In[92]:
 
 currDay = datetime.datetime.now().strftime("%Y%b%d")
-saveDir='./'
-fileName='PrimerSheet.csv'
-#print saveDir + fileName
-PrimerSheet.to_csv(saveDir + currDay + fileName,index=False)
+fileName = 'PrimerSheet.csv'
+PrimerSheet.to_csv(args.saveDir + currDay + fileName,index=False)
 
-
-# In[ ]:
 
 
 
