db2670a0951ce630d29bbeab9d3f6420ed0164a8
frickp <peter.l.frick@gmail.com>
2015-08-07 15:32:07 -0700

    Progress on figuring out V-plot script

--- VplotLearn/pyMakeVplot_PLF.py
+++ VplotLearn/pyMakeVplot_PLF.py
@@ -22,11 +22,150 @@ from multiprocessing import Pool
 from optparse import OptionParser
 import random
 
-bedFile = '~/gitHome/scriptsSU/VplotLearn/parsed_hg19_RefSeq.bed'
-bamFile = ''
+bedFile = './parsed_hg19_RefSeq.bed'
+#Bam file from /raid/Tn5_chromatin/150624_Peter*/plf-atac1atac2/PLF-1
+bamFile = './PLF-1_S6_L001_001.st.rmdup.flt.bam'
+baseExt=2000
+p1_ints = np.loadtxt(bedFile,'str') # bed file
+numThreads = 20
+
+##########################################################################################
+# script
+##########################################################################################
+
+# open and read BAM file
+bamfile = pysam.Samfile(bamFile, "rb")
+
+# determine number of rows and columns for matrix
+rows = 1000
+cols = int(baseExt)*2
+
+# split bedfile into chunks
+# This splits the bed file into smaller chunks to process. Size of chunks dependent on
+# processing power and size of bed file
+
+maxi=len(p1_ints)
+chunksize=maxi/int(numThreads)
+chunks=maxi/chunksize
+starts=range(0,maxi,chunksize)
+start=starts[0]
+
+mat = np.zeros([rows,cols])
+# loop through the intervals and get relevent info
+end=min(start+chunksize,len(p1_ints))
+# for i in range(start,end):
+i = 1
+
+# Define TSS-proximal regions of bed file to look for reads
+center = int(p1_ints[i][1])+(int(p1_ints[i][2])-int(p1_ints[i][1]))/2
+s_int=center-int(baseExt) #start
+e_int=center+int(baseExt) #end
+
+# Get the reads within the defined bed region using 'bamfile.fetch('chrN',bedRegStart,bedRegEnd)'
+
+# for p2_rds in bamfile.fetch(str(p1_ints[i][0]), max(0,s_int-2000), e_int+2000):
 
 
 
+
+def sub_Mat(start):
+    # initialize data matrix
+    mat = np.zeros([rows,cols])
+    # loop through the intervals and get relevent info
+    end=min(start+chunksize,len(p1_ints))
+    for i in range(start,end):
+        # get interval as num
+        center = int(p1_ints[i][1])+(int(p1_ints[i][2])-int(p1_ints[i][1]))/2
+        s_int=center-int(options.e) #start
+        e_int=center+int(options.e) #end
+        # loop through rds
+        for p2_rds in bamfile.fetch(str(p1_ints[i][0]), max(0,s_int-2000), e_int+2000):
+            #check mapping quality
+            if p2_rds.mapq<30:# or p2_rds.is_proper_pair==False:
+                continue
+            # get read positions
+            if p2_rds.is_reverse: #Why not use reverse strand reads?
+				continue
+            else:
+				l_pos = p2_rds.pos + 4 #In bam file
+        		        # calculate center point
+				ilen = abs(p2_rds.tlen) - 9
+        		        #ilen = 1
+				r_pos = l_pos+ilen
+				c_pos = l_pos + ilen / 2
+				if ilen%2 == 1 and options.p == 'center':
+					mat = asn_mat(c_pos,mat,s_int,e_int,ilen,i,0.5)
+					mat = asn_mat(c_pos+1,mat,s_int,e_int,ilen,i,0.5)
+				elif ilen%2 != 1 and options.p == 'center':
+					mat = asn_mat(c_pos,mat,s_int,e_int,ilen,i,1)
+				# save ends or read centers to v-plot
+				elif options.p == 'ends': #This flag is true in usage
+					mat = asn_mat(l_pos,mat,s_int,e_int,ilen,i,1)
+					mat = asn_mat(r_pos,mat,s_int,e_int,ilen,i,1)
+				else:
+					sys.exit('Error, check parameters')
+		    return mat
+
+
+
+
+
+
+# parallel processed computation of matrix for each chunk
+if __name__ == "__main__":
+    pool = Pool(processes=int(options.c))
+    sub_mats=pool.map(sub_Mat, starts,1)
+
+# sum up matrices for each chunk into matrix for all
+mat = np.zeros([rows,cols])
+for i in range(len(starts)):
+    mat=mat+sub_mats[i]
+
+# get column vector
+if options.v == True: #This is true in usage
+    mat = np.sum(mat,0)
+if options.i == True:
+    mat = np.sum(mat,1)
+
+# save matrix
+if not options.o: #options.o is $file/$file.RefSeqTSS in usage
+    n1=os.path.basename(options.a)
+    n2=os.path.basename(options.b)
+    if options.v == True: options.o=n1+'.'+n2+'.vect' 		#options.v is True in usage
+    elif options.i == True: options.o=n1+'.'+n2+'.iSize' 	#options.o is $file/$file.RefSeqTSS in usage
+    else: options.o=n1+'.'+n2+'.vplot' 						#options.o is $file/$file.RefSeqTSS in usage
+if options.u == True: #This is true in usage
+    np.savetxt(options.o,mat,delimiter='\t',fmt='%s') 		#options.o is $file/$file.RefSeqTSS in usage
+    np.save(options.o,mat)
+
+# plot
+fig=plt.figure(figsize=(8.0, 5.0))
+xran=min(500,int(options.e)) #options.3 is 2000 in usage
+yran=min(500,rows)
+if options.v == True: #This flag is true in usage
+    #plt.plot(mat/np.median(mat[1:200]))
+    plt.plot(mat/np.mean(mat[1:200]),'k.')
+    plt.plot(np.convolve(mat,np.ones(50),'same')/50/np.mean(mat[1:200]),'r')
+    plt.xlabel('Position relative to center')
+    plt.ylabel('Insertions')
+elif options.i == True:
+    plt.plot(mat[0:990])
+    plt.xlabel('Insert size (bp)')
+    plt.ylabel('Count')
+else:
+    plt.imshow(mat[0:yran,(int(options.e)-xran):(int(options.e)+xran+1)],origin='lower',aspect='equal',extent=[-xran,xran,1,yran+1])
+    plt.xlabel('Position relative to center')
+    plt.ylabel('Insert size')
+
+# save figure
+fig.savefig(options.o+'.png')
+plt.close(fig)
+
+##########################################################################################
+
+
+
+baseExt=2000
 
 
 
@@ -107,72 +246,6 @@ def sub_Mat(start):
 
 ##### INPUTS AND OUTPUTS #####
 # get intervals
-p1_ints = np.loadtxt(options.b,'str') # bed file
 
-##### SCRIPT #####
-# open and read BAM file
-bamfile = pysam.Samfile(options.a, "rb")
 
-# determine number of rows and columns for matrix
-rows = 1000
-cols = int(options.e)*2
-#cols = int(p1_ints[0][2])-int(p1_ints[0][1])+int(options.e)*2
 
-# split bedfile into chunks
-# This splits the bed file into smaller chunks to process. Size of chunks dependent on
-# processing power and size of bed file
-
-maxi=len(p1_ints)
-chunksize=maxi/int(options.c)
-chunks=maxi/chunksize
-starts=range(0,maxi,chunksize)
-
-# parallel processed computation of matrix for each chunk
-if __name__ == "__main__":
-    pool = Pool(processes=int(options.c))
-    sub_mats=pool.map(sub_Mat, starts,1)
-
-# sum up matrices for each chunk into matrix for all
-mat = np.zeros([rows,cols])
-for i in range(len(starts)):
-    mat=mat+sub_mats[i]
-
-# get column vector
-if options.v == True: #This is true in usage
-    mat = np.sum(mat,0)
-if options.i == True:
-    mat = np.sum(mat,1)
-
-# save matrix
-if not options.o: #options.o is $file/$file.RefSeqTSS in usage
-    n1=os.path.basename(options.a)
-    n2=os.path.basename(options.b)
-    if options.v == True: options.o=n1+'.'+n2+'.vect' 		#options.v is True in usage
-    elif options.i == True: options.o=n1+'.'+n2+'.iSize' 	#options.o is $file/$file.RefSeqTSS in usage
-    else: options.o=n1+'.'+n2+'.vplot' 						#options.o is $file/$file.RefSeqTSS in usage
-if options.u == True: #This is true in usage
-    np.savetxt(options.o,mat,delimiter='\t',fmt='%s') 		#options.o is $file/$file.RefSeqTSS in usage
-    np.save(options.o,mat)
-
-# plot
-fig=plt.figure(figsize=(8.0, 5.0))
-xran=min(500,int(options.e)) #options.3 is 2000 in usage
-yran=min(500,rows)
-if options.v == True: #This flag is true in usage
-    #plt.plot(mat/np.median(mat[1:200]))
-    plt.plot(mat/np.mean(mat[1:200]),'k.')
-    plt.plot(np.convolve(mat,np.ones(50),'same')/50/np.mean(mat[1:200]),'r')
-    plt.xlabel('Position relative to center')
-    plt.ylabel('Insertions')
-elif options.i == True:
-    plt.plot(mat[0:990])
-    plt.xlabel('Insert size (bp)')
-    plt.ylabel('Count')
-else:
-    plt.imshow(mat[0:yran,(int(options.e)-xran):(int(options.e)+xran+1)],origin='lower',aspect='equal',extent=[-xran,xran,1,yran+1])
-    plt.xlabel('Position relative to center')
-    plt.ylabel('Insert size')
-
-# save figure
-fig.savefig(options.o+'.png')
-plt.close(fig)
