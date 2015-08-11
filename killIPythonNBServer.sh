953c5d33d6f841575d72cc2303eaa1ba4423418b
frickp <peter.l.frick@gmail.com>
2015-08-11 14:12:07 -0700

    Updating remote IPython notebook scripts

--- killIPythonNBServer.sh
+++ killIPythonNBServer.sh
@@ -3,13 +3,35 @@
 # Linux tool to kill ipython notebook server on a remote server and
 # kill the current SSH tunnel for port forwarding
 #
-# Anthony Ho, ahho@stanford.edu, 2/20/2015
-# Last update 2/20/2015
+# Peter Frick, (modified from Anthony Ho), frickpl@stanford.edu
 
 
 # Before you run this, you will have set the environmental variable $greendragon as user@greendragon.stanford.edu
 # or replace $greendragon below as user@server.stanford.edu
-server=peter@greendragon.stanford.edu
+
+
+if [ -z $1 ]
+then
+	echo "Description:"
+	echo "Linux tool to stop ipython notebook server on a remote server"
+	echo ""
+	echo "Usage:"
+	echo "This script can toggle between greenseq and greendragon"
+	echo "To use greenseq, type any single word after the call, e.g.,       startIPythonNBServer.sh anyWord1"
+	echo "To use gdragon, type any two words after the call, e.g.,          startIPythonNBServer.sh anyWord1 anyWord2"
+	exit
+elif [ $# -ge 2 ]
+then
+	echo '2+ inputs detected. Closing greendragon...'
+	server=peter@greendragon.stanford.edu
+elif [ $# -ge 1 ]
+then	
+	echo '1 input detected. Closing greenseq'
+	server=peterfrick@greenseq.stanford.edu
+fi
+
+
+#server=peter@greendragon.stanford.edu
 remote_port=8889
 local_port=8888
 
--- startIPythonNBServer.sh
+++ startIPythonNBServer.sh
@@ -3,15 +3,36 @@
 # Linux tool to start ipython notebook server on a remote server and 
 # start a SSH tunnel for port forwarding
 #
-# Anthony Ho, ahho@stanford.edu, 2/20/2015
-# Last update 2/20/2015
+# Peter Frick, (modified from Anthony Ho), frickpl@stanford.edu
 
 
 # Before you run this, you will have set the environmental variable $greendragon as user@greendragon.stanford.edu
 # or replace $greendragon below as user@server.stanford.edu
 # server=$greendragon
 #server=peter@server.stanford.edu
-server=peter@greendragon.stanford.edu
+
+if [ -z $1 ]
+then
+	echo "Description:"
+	echo "Linux tool to start ipython notebook server on a remote server and"
+	echo "start an SSH tunnel for port forwarding"
+	echo ""
+	echo "Usage:"
+	echo "This script can toggle between greenseq and greendragon"
+	echo "To use greenseq, type any single word after the call, e.g.,       startIPythonNBServer.sh anyWord1"
+	echo "To use gdragon, type any two words after the call, e.g.,          startIPythonNBServer.sh anyWord1 anyWord2"
+	exit
+elif [ $# -ge 2 ]
+then
+	echo '2+ inputs detected. Opening greendragon...'
+	server=peter@greendragon.stanford.edu
+elif [ $# -ge 1 ]
+then	
+	echo '1 input detected. Opening greenseq'
+	server=peterfrick@greenseq.stanford.edu
+fi
+
+#server=peter@greendragon.stanford.edu
 remote_port=8889
 local_port=8888
 
@@ -21,6 +42,9 @@ ssh $server "nohup ipython notebook --pylab=inline --no-browser --port=$remote_p
 
 # Start SSH tunnel for port forwarding
 ssh -N -f -L localhost:$local_port:localhost:$remote_port $server
+
+echo "Now connected to " $HOSTNAME
+
 
 # Now you can access ipython notebook remotely via http://localhost:8888 on your favorite web browser
 # This functionality is added after the script has been ran on the terminal prompt (type the script name)
